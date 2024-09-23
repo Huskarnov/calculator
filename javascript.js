83b2cd33774a00f31162bd1de3ad7cd8cf4a04a9
Huskarnov <youcef.merad.batna2m@gmail.com>
2024-09-23 19:31:46 +0100

    x

--- javascript.js
+++ javascript.js
@@ -1,17 +1,30 @@
 
 let calcBody = document.querySelector(".calcBody");
 let input = document.querySelector(".screen");
-let operationFirst = false;
+let inputOp = "";
 
 calcBody.addEventListener("click", function(event){
     if(event.target.textContent === "C"){
-        input.value ="";
-        
+        inputOp = "";
+        input.value =inputOp;
     }
-    if(event.target.id ==="number"){
-        input.value += `${event.target.textContent}`;
-        
+    if(event.target.textContent === "DEL"){
+        inputOp = inputOp.slice(0, (inputOp.length-1));
+        input.value = inputOp;
     }
+    
+    if(input.value.length <= 11)
+        {
+            if(event.target.id ==="number"){
 
-    console.log(input.value);
+                inputOp += `${event.target.textContent}`;
+                input.value = inputOp;
+            }
+            
+            if(   event.target.id ==="operation" ){
+                inputOp += `${event.target.textContent}`; 
+                input.value = inputOp;
+            }
+        }
+    
 });
